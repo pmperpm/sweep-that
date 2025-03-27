7f8ebf1a4b84bb1acfaceb60a4bf39b1cff8a586
Swischya Sunthonphusit <swischya.s@ku.th>
2025-03-28 00:20:13 +0700

    score ui

--- sweepthat_game.py
+++ sweepthat_game.py
@@ -2,18 +2,17 @@
 import pygame
 from sweepthat_config import Config
 from sweepthat_classes import *
-from sweepthat_menu import *
+# from sweepthat_menu import *
 
-class Game(Menu):
+class Game():
     def __init__(self):
-        super().__init__()
         pygame.init()
         pygame.font.init()
         # self.asset = Asset()
         self.total_time = 0
         self.message_end_time = 0
-        self.user_score = 0
-        self.oppo_score = 0
+        self.user = User()
+        self.oppo = Opponent()
         self.oppo_acted = False
         self.count_time = 0
         self.start_time = 0
@@ -26,12 +25,13 @@ class Game(Menu):
         self.small_font = pygame.font.Font(Config.FONT_PATH, 30)
         self.medium_font = pygame.font.Font(Config.FONT_PATH, 50)
 
-
+        self.screen = pygame.display.set_mode((Config.WIDTH, Config.HEIGHT))
+        pygame.display.set_caption("SWEEP THAT Game")
         self.piece_manager = PieceManager(self.board)
         self.player_selected_card = None
         self.running = True
-        self.game_message, self.game_message_small,self.game_message_medium = "","", ""
-        self.time_message1, self.time_message2, self.time_message3, self.time_message4 = "","","",""
+        self.game_message, self.oppo_score_msg = "", ""
+        self.user_score_msg = ""
         self.last_click_time = pygame.time.get_ticks()  # Initialize with current time
         self.piece_manager.sound.play_sound()
 
@@ -61,7 +61,7 @@ class Game(Menu):
             self.game_message = ''
             self.game_message_small = ''
             self.message_end_time = 0
-            if self.user_score == 18 or self.oppo_score == 18:
+            if self.user.user_score == 18 or self.oppo.oppo_score == 18:
                 self.game_end()
             else:
                 self.continue_game()
@@ -78,6 +78,7 @@ class Game(Menu):
             # Background color
             # self.screen.fill(Config.COLORS["LIGHT_BROWN"])
             self.board.draw(self.screen)
+            self.user.draw(self.screen)
             # self.board.draw_card(self.screen)
 
 
@@ -113,16 +114,26 @@ class Game(Menu):
             msg_rect = msg_text.get_rect(center=(Config.WIDTH // 2, Config.HEIGHT // 2 + 20))
             self.screen.blit(msg_text, msg_rect)
 
-            msg_text_small = self.small_font.render(self.game_message_small, True, Config.COLORS["BLACK"])
-            msg_rect_small = msg_text_small.get_rect(center=(Config.WIDTH // 2, Config.HEIGHT // 2 + 80))
-            self.screen.blit(msg_text_small, msg_rect_small)
+            # msg_text_small = self.small_font.render(self.game_message_small, True, Config.COLORS["BLACK"])
+            # msg_rect_small = msg_text_small.get_rect(center=(Config.WIDTH // 2, Config.HEIGHT // 2 + 80))
+            # self.screen.blit(msg_text_small, msg_rect_small)
+
+            msg_user_score = self.small_font.render(str(self.user_score_msg), True, Config.COLORS["WHITE"])
+            msg_rect_user_score = msg_user_score.get_rect(center=(Config.WIDTH // 2 + 50, Config.HEIGHT // 2 + 120))
+            self.screen.blit(msg_user_score, msg_rect_user_score)
+            self.user_score_msg = self.user.user_score
+
+            msg_oppo_score = self.small_font.render(str(self.oppo_score_msg), True, Config.COLORS["WHITE"])
+            msg_rect_oppo_score = msg_oppo_score.get_rect(center=(Config.WIDTH // 2 + 50, Config.HEIGHT // 2 - 135))
+            self.screen.blit(msg_oppo_score, msg_rect_oppo_score)
+            self.oppo_score_msg = self.oppo.oppo_score
 
             ############## GAME PART #############
             if self.message_end_time != 0 and pygame.time.get_ticks() > self.message_end_time:
                 self.game_message = ''
                 self.game_message_small = ''
                 self.message_end_time = 0
-                if self.user_score == 18 or self.oppo_score == 18:
+                if self.user.user_score == 18 or self.oppo.oppo_score == 18:
                     self.game_end()
                 else:
                     self.continue_game()
@@ -130,8 +141,9 @@ class Game(Menu):
             # Check for inactivity (outside event loop)
             if self.player_selected_card is None:
                 if pygame.time.get_ticks() - self.last_click_time > 20000:
-                    self.oppo_score += 1
-                    print(f'Opponent correct!  Oppo score: {self.oppo_score}')
+                    self.oppo.oppo_score += 1
+                    self.oppo_score_msg = self.oppo.oppo_score
+                    print(f'Opponent correct!  Oppo score: {self.oppo.oppo_score}')
                     # Set the card and sound
                     # clicked_card.visible = False
                     self.piece_manager.board.cards[self.piece_manager.sound.correct_index].visible = False
@@ -141,7 +153,7 @@ class Game(Menu):
                     self.game_message_small = "game will start again in ..."
                     self.message_end_time = pygame.time.get_ticks() + 2000 # Display message
                     self.oppo_acted = True
-                    if self.user_score == 18 or self.oppo_score == 18:
+                    if self.user.user_score == 18 or self.oppo.oppo_score == 18:
                         self.game_end()
                     else:
                         self.continue_game()
@@ -179,14 +191,14 @@ class Game(Menu):
                             # sound_cor_idx = Sound.correct_index
                             # sound_list.remove(f'{sound_cor_idx}') # not working
                             # print(f'sound list : {sound_list}')
-
                             # Update message and score
                             self.game_message = 'Correct !'
                             self.game_message_small = 'game will start again in ...'
                             self.message_end_time = pygame.time.get_ticks() + 2000
                             pygame.mixer.music.stop()
-                            self.user_score += 1
-                            print(f'user score : {self.user_score}')
+                            self.user.user_score += 1
+                            print(f'user score : {self.user.user_score}')
+                            self.user_score_msg = self.user.user_score
 
                             # Reset selected card
                             self.player_selected_card = None
@@ -222,13 +234,13 @@ class Game(Menu):
 
     ####### adjust more ##########
     def game_end(self):
-        if self.user_score == 18:
+        if self.user.user_score == 18:
             self.game_message = 'YOU WIN !'
             self.game_message_small = 'great job ! Do you want to play again ?'
             # self.game_message_smaller = '> press R to restart the game'
             # self.game_message_smaller_2 = '> press m to go to the menu'
             # self.message_end_time = pygame.time.get_ticks() + 2000
-        elif self.oppo_score == 18:
+        elif self.oppo.oppo_score == 18:
             self.game_message = 'YOU LOSE !'
             self.game_message_small = 'better luck next time ! Do you want to play again ?'
             # self.message_end_time = pygame.time.get_ticks() + 2000
@@ -244,8 +256,8 @@ class Game(Menu):
 
     def restart_game(self):
         # Reset game state
-        self.user_score = 0
-        self.oppo_score = 0
+        self.user.user_score = 0
+        self.oppo.oppo_score = 0
         self.oppo_acted = False
         self.player_selected_card = None
         self.game_message, self.game_message_small = '', ''
